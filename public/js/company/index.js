b0ecf3d98aa37b15679c978f8a12e3ec6b0ee36d
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2021-05-30 16:45:57 +0300

    Add change page animation for interactive page wrapper and add video call wrapper

--- public/js/company/index.js
+++ public/js/company/index.js
@@ -12,8 +12,44 @@ function updateTargetUsersDetailsWrapper(user) {
   document.querySelector('.each-target-users-info-food').innerHTML = user.food;
 }
 
+function moveRightGradually(object, margin) {
+  const objectMargin = object.offsetLeft - object.parentNode.offsetLeft;
+
+  if (objectMargin >= margin)
+    return;
+
+  object.style.marginLeft = (objectMargin + 7) + 'px';
+
+  setTimeout(() => {
+    moveRightGradually(object, margin);
+  }, 1);
+}
+
+function moveLeftGradually(object, margin) {
+  const objectMargin = object.offsetLeft - object.parentNode.offsetLeft;
+
+  if (objectMargin <= margin)
+    return;
+
+  object.style.marginLeft = (objectMargin - 7) + 'px';
+
+  setTimeout(() => {
+    moveLeftGradually(object, margin);
+  }, 1);
+}
+
+function moveHorizontallyGradually(object, margin) {
+  margin = parseInt(margin);
+  const objectMargin = object.offsetLeft - object.parentNode.offsetLeft;
+
+  if (objectMargin > margin)
+    moveLeftGradually(object, margin);
+  else if (objectMargin < margin)
+    moveRightGradually(object, margin);
+}
+
 window.onload = () => {
-  // createPageListener(); // Call create page listener to listen the create page
+  createPageListener(); // Call create page listener to listen the create page
 
   const allHeader = document.querySelector('.all-header');
   const allInnerWrapper = document.querySelector('.all-inner-wrapper');
@@ -25,11 +61,44 @@ window.onload = () => {
 
   const targetUsers = JSON.parse(document.getElementById('target-users').value);
 
+  const darkBlueBackground = document.querySelector('.interactive-screen-background-dark-blue');
+  const blueBackground = document.querySelector('.interactive-screen-background-blue');
+  const borderLine = document.querySelector('.interactive-button-border-line');
+
   document.addEventListener('click', event => {
     if (event.target.classList.contains('each-target-user'))
       updateTargetUsersDetailsWrapper(targetUsers[event.target.id]);
     
     if (event.target.parentNode.classList.contains('each-target-user'))
       updateTargetUsersDetailsWrapper(targetUsers[event.target.parentNode.id]);
-  })
+    
+    if (event.target.classList.contains('interactive-create-survey-button') || event.target.parentNode.classList.contains('interactive-create-survey-button') || event.target.parentNode.parentNode.classList.contains('interactive-create-survey-button')) {
+      if (darkBlueBackground.classList.contains('opacity-decrease-animation-class')) {
+        blueBackground.classList.remove('opacity-decrease-animation-class');
+        blueBackground.classList.add('opacity-increase-animation-class');
+        darkBlueBackground.classList.remove('opacity-decrease-animation-class');
+        darkBlueBackground.classList.add('opacity-increase-animation-class');
+  
+        moveHorizontallyGradually(borderLine, 30);
+      } 
+    }
+
+    if (event.target.classList.contains('interactive-target-users-button') || event.target.parentNode.classList.contains('interactive-target-users-button') || event.target.parentNode.parentNode.classList.contains('interactive-target-users-button')) {
+      blueBackground.classList.remove('opacity-decrease-animation-class');
+      blueBackground.classList.add('opacity-increase-animation-class');
+      darkBlueBackground.classList.add('opacity-decrease-animation-class');
+      darkBlueBackground.classList.remove('opacity-increase-animation-class');
+
+      moveHorizontallyGradually(borderLine, borderLine.parentNode.offsetWidth/2 - 100);
+    }
+
+    if (event.target.classList.contains('interactive-analyze-results-button') || event.target.parentNode.classList.contains('interactive-analyze-results-button') || event.target.parentNode.parentNode.classList.contains('interactive-analyze-results-button')) {
+      blueBackground.classList.add('opacity-decrease-animation-class');
+      blueBackground.classList.remove('opacity-increase-animation-class');
+      darkBlueBackground.classList.add('opacity-decrease-animation-class');
+      darkBlueBackground.classList.remove('opacity-increase-animation-class');
+
+      moveHorizontallyGradually(borderLine, borderLine.parentNode.offsetWidth - 235);
+    }
+  });
 }
