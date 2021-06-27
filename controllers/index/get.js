9d01e358c5d1737e70cd01991b318bec7efa837f
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2021-06-27 10:18:51 +0300

    Add redirection based on request country

--- controllers/index/get.js
+++ controllers/index/get.js
@@ -1,5 +1,10 @@
-
 module.exports = (req, res) => {
+  const hostname = req.hostname;
+  const country = req.country;
+
+  if (country && hostname == 'usersmagic' && country == 'TR')
+    return res.redirect('https://tr.usersmagic.com');
+
   return res.render('index/index', {
     page: 'index/index',
     title: 'Unleash Your Users\' Magic',
