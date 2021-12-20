1ad362074014306006d6a3a1ea8976f10ed480e7
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2021-12-20 22:14:03 +0300

    -

--- controllers/index/get.js
+++ controllers/index/get.js
@@ -2,7 +2,7 @@
 module.exports = (req, res) => {
   return res.render('index/index', {
     page: 'index/index',
-    title: 'Hedef kitlenizi belirleyin',
+    title: 'Reach your target customer',
     meta: 'usersmagic',
     includes: {
       external: {
